8ad64d25ad61dce99d573df8aa9583aa1777ce04
Denwa799 <denwa799@yandex.ru>
2023-02-28 20:23:53 +0300

    Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react"
-import styles from './index.module.scss'
+import React from 'react';
+import styles from './index.module.scss';
 
 export const App = () => {
-    return <h1 className={styles.title}>Hello world!!!</h1>
-}
+  return <h1 className={styles.title}>Hello world!!!</h1>;
+};
